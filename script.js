e810e6aa3f1e2731ac3ce5a23cf839b17096a4ae
JustKardi2 <kpon033@learner.dvusd.org>
2025-01-29 21:02:12 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let type = new Typed("#sectionOneAutotyperAutotyper", {
+    strings: ["Inspire", "Empower", "Build"],
+    typeSpeed: 80,
+    backSpeed: 80,
+    loop: true,
+    loopCount: Infinity,
+    backDelay: 2000
+});
